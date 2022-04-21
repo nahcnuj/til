7e17e7f40ca322e6691cdd8b50d0e5326fef4812
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-22 07:38:25 +0900

    show addresses of balance for debugging
    the result of `go test`:
    
    address of balance in func is 0xc0000be0a0
    address of balance in test is 0xc0000be098
    --- FAIL: TestWallet (0.00s)
        wallet_test.go:19: got 0 want 10
    FAIL
    exit status 1
    FAIL	wallets	0.002s

--- learn-go-with-tests/wallets/wallet.go
+++ learn-go-with-tests/wallets/wallet.go
@@ -1,10 +1,13 @@
 package wallets
 
+import "fmt"
+
 type Wallet struct {
 	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in func is %v \n", &w.balance)
 	w.balance += amount
 }
 
