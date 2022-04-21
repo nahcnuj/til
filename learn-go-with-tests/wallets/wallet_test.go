551e23f7616b7002b8a26b6045a3bfb781714a02
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-22 07:58:05 +0900

    let a test fail in order to see a failure message
    the result of `go test`:
    
    address of balance in func is 0xc0000be098
    address of balance in test is 0xc0000be098
    --- FAIL: TestWallet (0.00s)
        wallet_test.go:19: got 10 BTC want 20 BTC
    FAIL
    exit status 1
    FAIL	wallets	0.003s

--- learn-go-with-tests/wallets/wallet_test.go
+++ learn-go-with-tests/wallets/wallet_test.go
@@ -13,7 +13,7 @@ func TestWallet(t *testing.T) {
 	got := wallet.Balance()
 	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
 
-	want := Bitcoin(10)
+	want := Bitcoin(20)
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
