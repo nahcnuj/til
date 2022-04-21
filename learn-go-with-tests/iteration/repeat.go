d54bbf2130783bac0e1a8cee447c1781ae7558e9
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-21 23:31:52 +0900

    take a new argument to resolve the failure [compilation fails]

--- learn-go-with-tests/iteration/repeat.go
+++ learn-go-with-tests/iteration/repeat.go
@@ -1,6 +1,6 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, times int) string {
 	var repeated string
 	for i := 0; i < 5; i++ {
 		repeated += character
