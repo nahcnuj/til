78f7a597c76e40e323198a40335d05ad1540e599
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-22 00:42:52 +0900

    rename the function

--- learn-go-with-tests/summation/sum.go
+++ learn-go-with-tests/summation/sum.go
@@ -8,7 +8,7 @@ func Sum(numbers []int) int {
 	return sum
 }
 
-func SumAll(numbersToSum ...[]int) (sums []int) {
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
 	for _, numbers := range numbersToSum {
 		sums = append(sums, Sum(numbers))
 	}
