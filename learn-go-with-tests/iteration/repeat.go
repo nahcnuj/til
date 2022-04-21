8561c06c5ccfd68a9edc4a39c7f3498979b17a65
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-21 23:34:05 +0900

    fix the loop condition

--- learn-go-with-tests/iteration/repeat.go
+++ learn-go-with-tests/iteration/repeat.go
@@ -2,7 +2,7 @@ package iteration
 
 func Repeat(character string, times int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < times; i++ {
 		repeated += character
 	}
 	return repeated
