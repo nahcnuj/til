fc9b4060069838c4bc023daef3bfbd54a94b56e6
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-22 06:26:35 +0900

    add a test to calculate the area of circles

--- learn-go-with-tests/shapes/shapes_test.go
+++ learn-go-with-tests/shapes/shapes_test.go
@@ -25,4 +25,14 @@ func TestArea(t *testing.T) {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := Area(circle)
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
