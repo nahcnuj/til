e44fb85fa957e5982496f4a5fc88e55f679f5897
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-21 22:02:38 +0900

    refactoring: define string constants

--- learn-go-with-tests/hello.go
+++ learn-go-with-tests/hello.go
@@ -2,14 +2,16 @@ package main
 
 import "fmt"
 
+const japanese = "Japanese"
 const englishHelloPrefix = "Hello, "
+const japaneseHelloPrefix = "こんにちは、"
 
 func Hello(name string, lang string) string {
 	if name == "" {
 		name = "World"
 	}
-	if lang == "Japanese" {
-		return "こんにちは、" + name
+	if lang == japanese {
+		return japaneseHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
