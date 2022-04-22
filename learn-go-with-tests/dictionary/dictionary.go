03f54dc5f4d60d4b7b2d168433c53c3d7d4bf1b1
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 02:04:59 +0900

    write minimal code to fix errors

--- learn-go-with-tests/dictionary/dictionary.go
+++ learn-go-with-tests/dictionary/dictionary.go
@@ -7,7 +7,10 @@ type Dictionary map[string]string
 // map is *a pointer* to a runtime.hmap structure
 // so copied receivers of a map can manipulate the values unlike struct
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add the word because it already exists")
+)
 
 func (d Dictionary) Search(key string) (string, error) {
 	value, ok := d[key]
@@ -17,6 +20,7 @@ func (d Dictionary) Search(key string) (string, error) {
 	return value, nil
 }
 
-func (d Dictionary) Add(key, value string) {
+func (d Dictionary) Add(key, value string) error {
 	d[key] = value
+	return nil
 }
