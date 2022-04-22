39dc41967eef4c81fa03b340c0c033245ca75653
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 04:45:16 +0900

    accept a Sleeper

--- learn-go-with-tests/countdown/countdown.go
+++ learn-go-with-tests/countdown/countdown.go
@@ -13,7 +13,7 @@ type Sleeper interface {
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
