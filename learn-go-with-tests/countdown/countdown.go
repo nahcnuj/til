6054cffbeb10caec4c8fe005a1c9091f4632a232
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 05:06:59 +0900

    fix the calls of Sleep
    This reverts commit bc1677e08d19839894b90701a249adf82e8051d8.

--- learn-go-with-tests/countdown/countdown.go
+++ learn-go-with-tests/countdown/countdown.go
@@ -22,9 +22,6 @@ const countdownStart = 3
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
-	}
-
-	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
 	sleeper.Sleep()
