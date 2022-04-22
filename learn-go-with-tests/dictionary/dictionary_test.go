17859f54873348f8f3dd119724737261ad081374
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-22 13:37:48 +0900

    refactoring

--- learn-go-with-tests/dictionary/dictionary_test.go
+++ learn-go-with-tests/dictionary/dictionary_test.go
@@ -9,7 +9,12 @@ func TestSearch(t *testing.T) {
 	got := Search(dict, key)
 	want := "This is just a test."
 
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, key)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
