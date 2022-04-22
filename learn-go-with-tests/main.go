3b38e43a5203291f27d7261439a8fe8a06236ba1
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 05:29:05 +0900

    use ConfigurableSleeper in main

--- learn-go-with-tests/main.go
+++ learn-go-with-tests/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"countdown"
 	"os"
+	"time"
 )
 
 func main() {
-	sleeper := &countdown.DefaultSleeper{}
+	sleeper := &countdown.ConfigurableSleeper{
+		Duration:  1 * time.Second,
+		SleepImpl: time.Sleep,
+	}
 	countdown.Countdown(os.Stdout, sleeper)
 }
