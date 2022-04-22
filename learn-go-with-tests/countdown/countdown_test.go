5cd6bb266fddb665cc7f0929d9d1caafa011f1ec
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 05:21:12 +0900

    create a test for ConfigurableSleeper

--- learn-go-with-tests/countdown/countdown_test.go
+++ learn-go-with-tests/countdown/countdown_test.go
@@ -85,3 +85,16 @@ type SpyTime struct {
 func (s *SpyTime) Sleep(duration time.Duration) {
 	s.durationSlept = duration
 }
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	sleptTime := spyTime.durationSlept
+	if sleptTime != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, sleptTime)
+	}
+}
