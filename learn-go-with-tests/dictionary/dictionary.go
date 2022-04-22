b328dc0d0363f1fb8ee719c22f5cb26cab0a57db
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-22 13:50:04 +0900

    check the word existence

--- learn-go-with-tests/dictionary/dictionary.go
+++ learn-go-with-tests/dictionary/dictionary.go
@@ -1,7 +1,13 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(key string) (string, error) {
-	return d[key], nil
+	value, ok := d[key]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+	return value, nil
 }
