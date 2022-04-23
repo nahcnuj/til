a06e3ccdd94fad9b22169f2b8ae055ac7b6a72a9
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 15:59:40 +0900

    commonize iterating over the fields of slice and struct

--- learn-go-with-tests/reflection/reflection.go
+++ learn-go-with-tests/reflection/reflection.go
@@ -5,17 +5,22 @@ import "reflect"
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
+	numberOfFields := 0
+	var getField func(int) reflect.Value
+
 	switch val.Kind() {
 	case reflect.String:
 		fn(val.String())
 	case reflect.Struct:
-		for i := 0; i < val.NumField(); i++ {
-			walk(val.Field(i).Interface(), fn)
-		}
+		numberOfFields = val.NumField()
+		getField = val.Field
 	case reflect.Slice:
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
+		numberOfFields = val.Len()
+		getField = val.Index
+	}
+
+	for i := 0; i < numberOfFields; i++ {
+		walk(getField(i).Interface(), fn)
 	}
 }
 
