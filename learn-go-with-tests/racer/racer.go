1f07b8d1aaa75a7d890ee504610ee23aa46d7bb0
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 13:45:25 +0900

    add a comment

--- learn-go-with-tests/racer/racer.go
+++ learn-go-with-tests/racer/racer.go
@@ -21,6 +21,7 @@ func measureResponseTime(url string) time.Duration {
 }
 
 func ping(url string) chan struct{} {
+	// need to make not to get nil, or it blocks forever because Go cannot send to it
 	ch := make(chan struct{}) // struct{} allocates nothing, is smaller than bool
 	go func() {
 		http.Get(url)
