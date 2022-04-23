63ef83c31729ad1320c95c65c4d02340fa18c567
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 20:38:17 +0900

    add a test for cancellation

--- learn-go-with-tests/contexts/context_test.go
+++ learn-go-with-tests/contexts/context_test.go
@@ -1,6 +1,7 @@
 package contexts
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -33,6 +34,26 @@ func TestServer(t *testing.T) {
 
 		if response.Body.String() != data {
 			t.Errorf("got %q, want %q", response.Body.String(), data)
+		}
+	})
+
+	t.Run("tell store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "Hello, world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Error("store was not told to cancel")
 		}
 	})
 }
