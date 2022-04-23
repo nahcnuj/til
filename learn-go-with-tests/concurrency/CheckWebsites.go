8362a5239f92a875d67e3f25b1aec374c09a75cb
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 12:33:07 +0900

    get results through a channel

--- learn-go-with-tests/concurrency/CheckWebsites.go
+++ learn-go-with-tests/concurrency/CheckWebsites.go
@@ -1,16 +1,25 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+
+type result struct {
+	string // url
+	bool   // result
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+
+	resultChannel := make(chan result)
 	for _, url := range urls { // the url variable is re-used
 		go func(u string) { // receives a copy of url to make sure the url is fixed for each iteration
-			results[u] = wc(u)
+			resultChannel <- result{u, wc(u)}
 		}(url)
 	}
-	time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
+	}
+
 	return results
 }
