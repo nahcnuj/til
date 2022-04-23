80d1c8b7c30308fa097572def91ca3cced759c95
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 12:07:59 +0900

    add a comment

--- learn-go-with-tests/concurrency/CheckWebsites.go
+++ learn-go-with-tests/concurrency/CheckWebsites.go
@@ -6,7 +6,7 @@ type WebsiteChecker func(string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
-	for _, url := range urls {
+	for _, url := range urls { // the url variable is re-used
 		go func() {
 			results[url] = wc(url)
 		}()
