977b78b20ea8b2445f5be87097a1b11616791930
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 18:15:58 +0900

    add a test of asynchronous version

--- learn-go-with-tests/synchronous/sync_test.go
+++ learn-go-with-tests/synchronous/sync_test.go
@@ -1,6 +1,9 @@
 package synchronous
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("increment the counter 3 times, leaveing it at 3", func(t *testing.T) {
@@ -10,6 +13,24 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
+	})
+
+	t.Run("run safely concurrently", func(t *testing.T) {
+		wantedCount := 1_000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
 	})
 }
 
