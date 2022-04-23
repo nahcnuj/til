792d9239ed30d3e5d342bbb2de4f5e3ef8e42d91
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 12:13:10 +0900

    pass a copy of the url to goroutine

--- learn-go-with-tests/concurrency/CheckWebsites.go
+++ learn-go-with-tests/concurrency/CheckWebsites.go
@@ -7,9 +7,9 @@ type WebsiteChecker func(string) bool
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 	for _, url := range urls { // the url variable is re-used
-		go func() {
-			results[url] = wc(url)
-		}()
+		go func(u string) { // receives a copy of url to make sure the url is fixed for each iteration
+			results[u] = wc(u)
+		}(url)
 	}
 	time.Sleep(2 * time.Second)
 	return results
