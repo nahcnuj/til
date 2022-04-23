779840024e401021e989f7576f5ce49f9d3dc48d
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-23 13:57:56 +0900

    time out if it takes 10 seconds

--- learn-go-with-tests/racer/racer.go
+++ learn-go-with-tests/racer/racer.go
@@ -1,6 +1,7 @@
 package racer
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -11,6 +12,8 @@ func Racer(a, b string) (winner string, err error) {
 		return a, nil
 	case <-ping(b):
 		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
