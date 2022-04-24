a24fd8cb8f531b1d17c177e67a5a6431aa030e9a
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-24 13:27:13 +0900

    add a property based test, but...

--- learn-go-with-tests/numeral/numeral_test.go
+++ learn-go-with-tests/numeral/numeral_test.go
@@ -3,6 +3,7 @@ package numeral
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 )
 
 var cases = []struct {
@@ -60,5 +61,17 @@ func TestConvertingToArabic(t *testing.T) {
 				t.Errorf("got %d, want %d", got, test.Arabic)
 			}
 		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic int) bool {
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
 	}
 }
