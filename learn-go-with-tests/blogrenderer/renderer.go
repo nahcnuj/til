0c9490c2d4d91610c583a3d90338d7f21af099c2
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-24 20:23:11 +0900

    render more information in HTML

--- learn-go-with-tests/blogrenderer/renderer.go
+++ learn-go-with-tests/blogrenderer/renderer.go
@@ -7,6 +7,25 @@ import (
 )
 
 func Render(w io.Writer, post blogposts.Post) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1>", post.Title)
+	_, err := fmt.Fprintf(w, "<h1>%s</h1>\n", post.Title)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "<p>%s</p>\n", post.Description)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "Tags: <ul>")
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range post.Tags {
+		fmt.Fprintf(w, "<li>%s</li>", tag)
+	}
+
+	_, err = fmt.Fprintf(w, "</ul>")
 	return err
 }
