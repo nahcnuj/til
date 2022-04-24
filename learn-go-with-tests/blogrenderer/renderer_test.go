df4c7c150ef42b0ec4e4cf54b9cdc278a99a049e
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-24 20:04:24 +0900

    write a renderer test

---
+++ learn-go-with-tests/blogrenderer/renderer_test.go
@@ -0,0 +1,31 @@
+package blogrenderer_test
+
+import (
+	"blogposts"
+	"bytes"
+	"testing"
+)
+
+func TestRender(t *testing.T) {
+	var aPost = blogposts.Post{
+		Title:       "Hello, world",
+		Description: "This is a description.",
+		Tags:        []string{"go", "TDD"},
+		Body:        "This is a blog post.",
+	}
+
+	t.Run("convert a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		err := blogrenderer.Render(&buf, aPost)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<h1>Hello, world</h1>`
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
