d03de216df5ba9af37f2471c7b69ca6ef5ce001b
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-24 11:05:29 +0900

    decouple the data (when and which symbol to add) from behaviour (appending to string and substracting)

--- learn-go-with-tests/numeral/numeral.go
+++ learn-go-with-tests/numeral/numeral.go
@@ -2,25 +2,25 @@ package numeral
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var breakpoints = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
-	for arabic > 0 {
-		switch {
-		case arabic > 9:
-			result.WriteString("X")
-			arabic -= 10
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic--
+	for _, breakpoint := range breakpoints {
+		for arabic >= breakpoint.Value {
+			result.WriteString(breakpoint.Symbol)
+			arabic -= breakpoint.Value
 		}
 	}
 	return result.String()
