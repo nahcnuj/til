cc888ee432ac6d8bc4d4baafb7b20f2937d72b91
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-24 22:41:58 +0900

    add a benchmark test
    on my computer:
    
    goos: linux
    goarch: amd64
    pkg: blogrenderer
    cpu: Intel(R) Core(TM) i5-4210U CPU @ 1.70GHz
    BenchmarkRender-4   	    5018	    216791 ns/op
    PASS
    ok  	blogrenderer	1.122s

--- learn-go-with-tests/blogrenderer/renderer_test.go
+++ learn-go-with-tests/blogrenderer/renderer_test.go
@@ -4,6 +4,7 @@ import (
 	"blogposts"
 	"blogrenderer"
 	"bytes"
+	"io"
 	"testing"
 
 	approvals "github.com/approvals/go-approval-tests"
@@ -27,4 +28,18 @@ func TestRender(t *testing.T) {
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var aPost = blogposts.Post{
+		Title:       "Hello, world",
+		Description: "This is a description.",
+		Tags:        []string{"go", "TDD"},
+		Body:        "This is a blog post.",
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		blogrenderer.Render(io.Discard, aPost)
+	}
 }
