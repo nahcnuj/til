44488767304d651eb0fe6fc45f466245946307d8
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-24 23:02:14 +0900

    add a test for rendering index of posts

--- learn-go-with-tests/blogrenderer/renderer_test.go
+++ learn-go-with-tests/blogrenderer/renderer_test.go
@@ -33,6 +33,22 @@ func TestRender(t *testing.T) {
 
 		approvals.VerifyString(t, buf.String())
 	})
+
+	t.Run("render an index of posts", func(t *testing.T) {
+		buf := bytes.Buffer{}
+		posts := []blogposts.Post{{Title: "Hello World"}, {Title: "Hello World 2"}}
+
+		if err := postRenderer.RenderIndex(&buf, posts); err != nil {
+			t.Fatal(err)
+		}
+
+		got := buf.String()
+		want := `<ol><li><a href="/post/hello-world">Hello World</a></li><li><a href="/post/hello-world-2">Hello World 2</a></li></ol>`
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
 
 func BenchmarkRender(b *testing.B) {
