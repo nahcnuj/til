b2a32a329d4efa7f6cd0844c424819da0929f622
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-24 10:35:14 +0900

    use the Builder for 4

--- learn-go-with-tests/numeral/numeral.go
+++ learn-go-with-tests/numeral/numeral.go
@@ -3,12 +3,12 @@ package numeral
 import "strings"
 
 func ConvertToRoman(arabic int) string {
-	if arabic == 4 {
-		return "IV"
-	}
-
 	var result strings.Builder
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 	return result.String()
