0a6cc7f5140e293710332fa66503992e91b9a526
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-24 16:22:24 +0900

    return nil as an error

--- learn-go-with-tests/blogposts/blogposts.go
+++ learn-go-with-tests/blogposts/blogposts.go
@@ -7,11 +7,11 @@ import (
 type Post struct {
 }
 
-func FromFS(fsys fs.FS) []Post {
+func FromFS(fsys fs.FS) ([]Post, error) {
 	dir, _ := fs.ReadDir(fsys, ".")
 	var posts []Post
 	for range dir {
 		posts = append(posts, Post{})
 	}
-	return posts
+	return posts, nil
 }
