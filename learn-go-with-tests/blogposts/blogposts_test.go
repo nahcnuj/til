8685224679353066aeab20845a9eb8c9de05d649
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-24 16:28:11 +0900

    add a case that ReadDir fails, following the TDD manner properly

--- learn-go-with-tests/blogposts/blogposts_test.go
+++ learn-go-with-tests/blogposts/blogposts_test.go
@@ -1,6 +1,8 @@
 package blogposts_test // appending "_test" to make tests closer to real usage
 
 import (
+	"errors"
+	"io/fs"
 	"testing"
 	"testing/fstest"
 
@@ -8,18 +10,34 @@ import (
 )
 
 func TestNewBlogPosts(t *testing.T) {
-	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hello")},
-		"hello-world2.md": {Data: []byte("コニチハ")},
-	}
+	t.Run("create posts from FS", func(t *testing.T) {
+		fs := fstest.MapFS{
+			"hello world.md":  {Data: []byte("hello")},
+			"hello-world2.md": {Data: []byte("コニチハ")},
+		}
 
-	posts, err := blogposts.FromFS(fs)
+		posts, err := blogposts.FromFS(fs)
 
-	if err != nil {
-		t.Fatal(err)
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	if len(posts) != len(fs) {
-		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
-	}
+		if len(posts) != len(fs) {
+			t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+		}
+	})
+
+	t.Run("return an error if ReadDir fails", func(t *testing.T) {
+		_, err := blogposts.FromFS(StubFailingFS{})
+
+		if err == nil {
+			t.Error("expected an error but did not get one")
+		}
+	})
+}
+
+type StubFailingFS struct{}
+
+func (s StubFailingFS) Open(name string) (fs.File, error) {
+	return nil, errors.New("something went wrong")
 }
