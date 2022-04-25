171cefca63192b7a0348c0e97335edbd3cd85f48
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-25 09:30:22 +0900

    write test code for the first step

---
+++ learn-go-with-tests/my-app/server_test.go
@@ -0,0 +1,24 @@
+package my_app
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetPlayers(t *testing.T) {
+	t.Run("return Pepper's score", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "20"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+}
