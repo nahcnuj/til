2728af390916c21ac88c77812e953672370940bf
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-25 23:53:29 +0900

    add a field that records RecordWin method calls into the stub

--- learn-go-with-tests/my-app/server_test.go
+++ learn-go-with-tests/my-app/server_test.go
@@ -8,12 +8,17 @@ import (
 )
 
 type StubPlayerStore struct {
-	scores map[string]int
+	scores   map[string]int
+	winCalls []string
 }
 
 func (s *StubPlayerStore) GetPlayerScore(player string) int {
 	score := s.scores[player]
 	return score
+}
+
+func (s *StubPlayerStore) RecordWin(player string) {
+	s.winCalls = append(s.winCalls, player)
 }
 
 func TestGetPlayers(t *testing.T) {
@@ -22,6 +27,7 @@ func TestGetPlayers(t *testing.T) {
 			"Pepper": 20,
 			"Floyd":  10,
 		},
+		nil,
 	}
 	server := &PlayerServer{&store}
 
@@ -56,16 +62,23 @@ func TestGetPlayers(t *testing.T) {
 }
 
 func TestScoreWins(t *testing.T) {
-	store := StubPlayerStore{map[string]int{}}
+	store := StubPlayerStore{
+		map[string]int{},
+		nil,
+	}
 	server := NewServer(&store)
 
-	t.Run("return accepted on POST", func(t *testing.T) {
+	t.Run("record wins when POST", func(t *testing.T) {
 		request := httptest.NewRequest(http.MethodPost, "/players/Pepper", nil)
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
 
 		assertStatus(t, response.Code, http.StatusAccepted)
+
+		if len(store.winCalls) != 1 {
+			t.Errorf("got %d calls to RecordWin", len(store.winCalls))
+		}
 	})
 }
 
