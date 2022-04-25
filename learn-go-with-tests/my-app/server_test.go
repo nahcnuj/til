f5e4183810c5e2233978d7185727168729330c5a
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-25 22:18:42 +0900

    add a test

--- learn-go-with-tests/my-app/server_test.go
+++ learn-go-with-tests/my-app/server_test.go
@@ -21,4 +21,17 @@ func TestGetPlayers(t *testing.T) {
 		}
 	})
 
+	t.Run("return Floyd's score", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		got := response.Body.String()
+		want := "10"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }
