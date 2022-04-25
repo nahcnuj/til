f785917ddf20345ae920a9e767c2de275d02cc4c
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-25 09:37:11 +0900

    write "20" in the response

--- learn-go-with-tests/my-app/server.go
+++ learn-go-with-tests/my-app/server.go
@@ -1,7 +1,10 @@
 package my_app
 
 import (
+	"fmt"
 	"net/http"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {}
+func PlayerServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "20")
+}
