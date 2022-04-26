65556d7c948570093da4d16b45b7cf3b7ec12af7
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-26 23:30:50 +0900

    add a concurrent test

---
+++ learn-go-with-tests/my-app/in_memory_player_store_test.go
@@ -0,0 +1,28 @@
+package my_app
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestStoreConcurrently(t *testing.T) {
+	wanted_count := 1_000
+	player := "Pepper"
+	store := NewInMemoryPlayerStore()
+
+	var wg sync.WaitGroup
+	wg.Add(wanted_count)
+
+	for i := 0; i < wanted_count; i++ {
+		go func() {
+			store.RecordWin(player)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+
+	got := store.GetPlayerScore(player)
+	if got != wanted_count {
+		t.Errorf("got %d wins, but want %d wins", got, wanted_count)
+	}
+}
