fc2689c605aec0b2c8ebfc88ea60bcbb9af14296
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-04-29 13:30:14 +0900

    add a check of Content-Type header

--- learn-go-with-tests/my-app/server_test.go
+++ learn-go-with-tests/my-app/server_test.go
@@ -116,6 +116,10 @@ func TestLeague(t *testing.T) {
 		got := getLeagueFromResponse(t, response.Body)
 		assertStatus(t, http.StatusOK, response.Code)
 		assertLeague(t, got, wantedLeague)
+
+		if response.Result().Header.Get("content-type") != "application/json" {
+			t.Errorf("response did not have content-type of application/json, got %v", response.Result().Header)
+		}
 	})
 }
 
