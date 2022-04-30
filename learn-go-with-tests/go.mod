3ca55f5157360f748feddbb8523d41bd5bfe82f9
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-05-01 00:34:58 +0900

    replace in-memory store of app with file system one

--- learn-go-with-tests/go.mod
+++ learn-go-with-tests/go.mod
@@ -2,6 +2,6 @@ module github.com/nahcnuj/til/learn-go-with-tests
 
 go 1.18
 
-require github.com/nahcnuj/til/learn-go-with-tests/my-app v0.0.0-20220426143611-f1d268e952c8
+require github.com/nahcnuj/til/learn-go-with-tests/my-app v0.0.0-20220430143031-bd195ec5ce5f
 
 replace github.com/nahcnuj/til/learn-go-with-tests/my-app => ./my-app
--- learn-go-with-tests/main.go
+++ learn-go-with-tests/main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	app "github.com/nahcnuj/til/learn-go-with-tests/my-app"
 )
 
+const dbFileName = "db.json"
+
 func main() {
-	server := app.NewServer(app.NewInMemoryPlayerStore())
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store := app.NewFileSystemPlayerStore(db)
+	server := app.NewServer(store)
+
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
