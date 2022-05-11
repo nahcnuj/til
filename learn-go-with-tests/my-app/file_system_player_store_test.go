d9cd7315d604d56ae63136aa8bd8cd1ce2c7ec20
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-05-12 00:19:00 +0900

    add a test for empty file

--- learn-go-with-tests/my-app/file_system_player_store_test.go
+++ learn-go-with-tests/my-app/file_system_player_store_test.go
@@ -78,6 +78,14 @@ func TestFileSystemPlayerStore(t *testing.T) {
 		want := 1
 		assertScoreEquals(t, got, want)
 	})
+
+	t.Run("works with an empty file", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, "")
+		defer cleanDatabase()
+
+		_, err := NewFileSystemPlayerStore(database)
+		assertNoError(t, err)
+	})
 }
 
 func assertScoreEquals(t testing.TB, got, want int) {
