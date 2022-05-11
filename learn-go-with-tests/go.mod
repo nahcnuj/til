9950ce2c799b37c9f713336d08b3ea0b60c49bf1
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-05-11 23:12:55 +0900

    also fix main.go

--- learn-go-with-tests/go.mod
+++ learn-go-with-tests/go.mod
@@ -2,6 +2,6 @@ module github.com/nahcnuj/til/learn-go-with-tests
 
 go 1.18
 
-require github.com/nahcnuj/til/learn-go-with-tests/my-app v0.0.0-20220430143031-bd195ec5ce5f
+require github.com/nahcnuj/til/learn-go-with-tests/my-app v0.0.0-20220511140758-faea4a475079
 
 replace github.com/nahcnuj/til/learn-go-with-tests/my-app => ./my-app
--- learn-go-with-tests/main.go
+++ learn-go-with-tests/main.go
@@ -16,8 +16,11 @@ func main() {
 		log.Fatalf("problem opening %s %v", dbFileName, err)
 	}
 
-	store := app.NewFileSystemPlayerStore(db)
-	server := app.NewServer(store)
+	store, err := app.NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatalf("problem loading to player store from file, %v", err)
+	}
 
+	server := app.NewServer(store)
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
