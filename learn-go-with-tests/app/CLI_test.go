e7dd1acb3311a9549ab28c397b4e13aca9485b7b
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-05-12 01:40:32 +0900

    simulate stdin by strings.Reader in the test

--- learn-go-with-tests/app/CLI_test.go
+++ learn-go-with-tests/app/CLI_test.go
@@ -1,13 +1,24 @@
 package app
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestCLI(t *testing.T) {
+	in := strings.NewReader("Chris wins\n")
 	store := &StubPlayerStore{}
-	cli := &CLI{store}
+	cli := &CLI{store, in}
 	cli.PlayPoker()
 
 	if len(store.winCalls) != 1 {
 		t.Fatal("expected a win call but didn't call")
+	}
+
+	got := store.winCalls[0]
+	want := "Chris"
+
+	if got != want {
+		t.Errorf("didn't record correct winner, got %q, want %q", got, want)
 	}
 }
