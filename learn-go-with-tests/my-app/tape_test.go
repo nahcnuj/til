3250b1757febdd584f1baba1cbfc3e388dca6fbc
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-05-11 22:36:09 +0900

    add a test that writes shorter content to the tape

---
+++ learn-go-with-tests/my-app/tape_test.go
@@ -0,0 +1,28 @@
+package my_app
+
+import (
+	"io"
+	"io/ioutil"
+	"testing"
+)
+
+func TestTape_Write(t *testing.T) {
+	file, clean := createTempFile(t, "12345")
+	defer clean()
+
+	newText := "abc"
+
+	tape := &tape{file}
+
+	tape.Write([]byte(newText))
+
+	file.Seek(0, io.SeekStart)
+	newFileContents, _ := ioutil.ReadAll(file)
+
+	got := string(newFileContents)
+	want := newText
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
