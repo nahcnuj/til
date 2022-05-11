0f0a0f80fa6335c513c2f87b0bc7d76c5e893815
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-05-12 01:57:47 +0900

    write code in main.go of cli

--- learn-go-with-tests/app/cmd/cli/main.go
+++ learn-go-with-tests/app/cmd/cli/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/nahcnuj/til/learn-go-with-tests/app"
+)
+
+const dbFileName = "db.json"
 
 func main() {
 	fmt.Println("Let's play poker")
+	fmt.Println("Type '{Name} wins' to record a win")
+
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("could not open %s, %v", dbFileName, err)
+	}
+
+	store, err := app.NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatalf("could not create player store from the file, %v", err)
+	}
+
+	game := app.CLI{store, os.Stdin}
+	game.PlayPoker()
 }
