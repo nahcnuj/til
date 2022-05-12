d4fdd46543b2db157d124b1e6cba39b5e3b0cd33
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-05-13 04:40:34 +0900

    remove tests about internal of game because moved them into game_test

--- learn-go-with-tests/app/CLI_test.go
+++ learn-go-with-tests/app/CLI_test.go
@@ -2,10 +2,8 @@ package app_test
 
 import (
 	"bytes"
-	"fmt"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/nahcnuj/til/learn-go-with-tests/app"
 )
@@ -46,39 +44,15 @@ func TestCLI(t *testing.T) {
 		assertWinner(t, game.FinishedWith, "Cleo")
 	})
 
-	t.Run("schedule printing of blind values for 5 players", func(t *testing.T) {
+	t.Run("start with 5 players and finish", func(t *testing.T) {
 		in := strings.NewReader("5\nChris wins\n")
-		store := &app.StubPlayerStore{}
-		blindAlerter := &SpyBlindAlerter{}
-		game := app.NewTexasHoldem(store, blindAlerter)
+		game := &SpyGame{}
 
 		cli := app.NewCLI(in, dummyStdOut, game)
 		cli.PlayPoker()
 
-		cases := []scheduledAlert{
-			{0 * time.Minute, 100},
-			{10 * time.Minute, 200},
-			{20 * time.Minute, 400},
-			{30 * time.Minute, 600},
-			{40 * time.Minute, 1000},
-			{50 * time.Minute, 2000},
-			{60 * time.Minute, 4000},
-			{70 * time.Minute, 8000},
-			{80 * time.Minute, 16000},
-			{90 * time.Minute, 32000},
-			{100 * time.Minute, 64000},
-		}
-
-		for i, want := range cases {
-			t.Run(fmt.Sprintf("%d scheduled for %v", want.amount, want.at), func(t *testing.T) {
-				if len(blindAlerter.alerts) <= i {
-					t.Fatalf("alert #%d was not scheduled, %v", i, blindAlerter.alerts)
-				}
-
-				got := blindAlerter.alerts[i]
-				assertScheduledAlert(t, got, want)
-			})
-		}
+		assertNumberOfPlayers(t, game.StartedWith, 5)
+		assertWinner(t, game.FinishedWith, "Chris")
 	})
 
 	t.Run("prompt the user to enter the number of players first", func(t *testing.T) {
@@ -96,10 +70,15 @@ func TestCLI(t *testing.T) {
 			t.Errorf("wrong prompt, got %q, want %q", gotPrompt, wantPrompt)
 		}
 
-		if game.StartedWith != 7 {
-			t.Errorf("expected 7 players but got %d", game.StartedWith)
-		}
+		assertNumberOfPlayers(t, game.StartedWith, 7)
 	})
+}
+
+func assertNumberOfPlayers(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("expected %d players, but got %d", want, got)
+	}
 }
 
 func assertWinner(t testing.TB, got, want string) {
