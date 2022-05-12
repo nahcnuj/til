7c9a6431226bd39375e289945e723bd0220f1b6f
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-05-13 02:56:01 +0900

    add a test for 7 players

--- learn-go-with-tests/app/CLI_test.go
+++ learn-go-with-tests/app/CLI_test.go
@@ -92,6 +92,40 @@ func TestCLI(t *testing.T) {
 			t.Errorf("wrong prompt, got %q, want %q", got, want)
 		}
 	})
+
+	t.Run("schedule printing of blind values for 7 players", func(t *testing.T) {
+		in := strings.NewReader("7\n")
+		stdout := &bytes.Buffer{}
+		blindAlerter := &SpyBlindAlerter{}
+		cli := app.NewCLI(dummyPlayerStore, in, stdout, blindAlerter)
+		cli.PlayPoker()
+
+		got := stdout.String()
+		want := app.PlayerPrompt
+
+		if got != want {
+			t.Errorf("wrong prompt, got %q, want %q", got, want)
+		}
+
+		cases := []scheduledAlert{
+			{0 * time.Minute, 100},
+			{12 * time.Minute, 200},
+			{24 * time.Minute, 400},
+			{36 * time.Minute, 600},
+			{48 * time.Minute, 1000},
+			{60 * time.Minute, 2000},
+		}
+		for i, want := range cases {
+			t.Run(fmt.Sprint(want), func(t *testing.T) {
+				if len(blindAlerter.alerts) <= i {
+					t.Fatalf("alert #%d was not scheduled, %v", i, blindAlerter.alerts)
+				}
+
+				got := blindAlerter.alerts[i]
+				assertScheduledAlert(t, got, want)
+			})
+		}
+	})
 }
 
 func assertScheduledAlert(t testing.TB, got, want scheduledAlert) {
