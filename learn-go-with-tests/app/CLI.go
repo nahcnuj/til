1d54608561e1ae799b96ddb68db88c87dec2181d
Junichi Hayashi <2093896+nahcnuj@users.noreply.github.com>
2022-05-13 02:00:35 +0900

    extract scheduling alerts into a method

--- learn-go-with-tests/app/CLI.go
+++ learn-go-with-tests/app/CLI.go
@@ -22,15 +22,18 @@ func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
 }
 
 func (cli *CLI) PlayPoker() {
+	cli.scheduleBlindAlerts()
+	userInput := cli.readLine()
+	cli.store.RecordWin(extractWinner(userInput))
+}
+
+func (cli *CLI) scheduleBlindAlerts() {
 	blinds := []int{100, 200, 400, 600, 1000, 2000, 4000, 8000, 16000, 32000, 64000}
 	blindTime := 0 * time.Minute
 	for _, amount := range blinds {
 		cli.alerter.ScheduleAlertAt(blindTime, amount)
 		blindTime += 10 * time.Minute
 	}
-
-	userInput := cli.readLine()
-	cli.store.RecordWin(extractWinner(userInput))
 }
 
 func extractWinner(input string) string {
